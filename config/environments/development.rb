69740c8897edd209c57b961afa6ec2041f7e7a7d
darthmolly <mpc3c@virginia.edu>
2011-12-12 15:17:38 -0500

    rails3-ified environment files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,48 +1,36 @@
-# Settings specified here will take precedence over those in config/environment.rb
+Virgo::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb
 
-# In the development environment your application's code is reloaded on
-# every request.  This slows down response time but is perfect for development
-# since you don't have to restart the webserver when you make code changes.
-config.cache_classes = true
+  # In the development environment your application's code is reloaded on
+  # every request.  This slows down response time but is perfect for development
+  # since you don't have to restart the webserver when you make code changes.
+  config.cache_classes = false
 
-# Log error messages when you accidentally call methods on nil.
-config.whiny_nils = true
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
 
-# Show full error reports and disable caching
-config.action_controller.consider_all_requests_local = true
-config.action_view.debug_rjs                         = true
-config.action_controller.perform_caching             = false
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+  config.action_view.debug_rjs             = true
+  config.action_controller.perform_caching = false
 
-config.action_mailer.delivery_method = :smtp
-# Don't care if the mailer can't send
-config.action_mailer.raise_delivery_errors = true
+  # Don't care if the mailer can't send
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = false
 
+  # Print deprecation notices to the Rails logger
+  config.active_support.deprecation = :log
 
-config.after_initialize do
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = { 
-    :address => 'smtp.mail.virginia.edu',
-    :domain => 'd-128-197-140.bootp.Virginia.EDU'
-  }
-  ActionView::Base.field_error_proc = Proc.new { |html_tag, instance|
-  "<span class=\"fieldWithErrors\">#{html_tag}</span>" }
-  ExceptionNotification::Notifier.exception_recipients = %w(mpc3c@virginia.edu)  
-  ExceptionNotification::Notifier.sender_address = %("Error" <mpc3c@virginia.edu>)
-  ExceptionNotification::Notifier.email_prefix = "[development] "
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
+
+  FEEDBACK_NOTIFICATION_RECIPIENTS = %W(mpc3c@virginia.edu)
+  FEEDBACK_NOTIFICATION_FROM = 'virgo-feedback@virginia.edu'
+  FEEDBACK_NOTIFICATION_SUBJECT = 'Virgo Feedback (dev)'
+
+  FIREHOSE_URL = "http://firehose.lib.virginia.edu:8080/firehose2"
+  PRIMO_URL = "http://primo4.hosted.exlibrisgroup.com:1701/PrimoWebServices/xservice/search/brief?institution=UVA&onCampus=true"
+  
 end
 
-# API KEYS FOR UVA #
-LIBRARY_THING_API_KEY = '73d3097200e332adbe542b6eb7fdb162'
-LAST_FM_API_KEY = 'b25b959554ed76058ac220b7b2e0a026'
 
-
-FEEDBACK_NOTIFICATION_RECIPIENTS = %W(mpc3c@virginia.edu)
-FEEDBACK_NOTIFICATION_FROM = 'eos8d@virginia.edu'
-FEEDBACK_NOTIFICATION_SUBJECT = 'VIRGOBeta Feedback (development)'
-
-FEDORA_REST_URL = 'http://localhost:8080/fedora'
-Fedora_username = 'fedoraAdmin'
-Fedora_password = 'fedoraAdmin'
-
-FIREHOSE_URL = "http://webservice.lib.virginia.edu:8080/firehose2"
-PRIMO_URL = "http://primo4.hosted.exlibrisgroup.com:1701/PrimoWebServices/xservice/search/brief?institution=UVA&onCampus=true"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,46 +1,65 @@
-# Settings specified here will take precedence over those in config/environment.rb
+Virgo::Application.configure do
 
-# The production environment is meant for finished, "live" apps.
-# Code is not reloaded between requests
-config.cache_classes = true
+  # Settings specified here will take precedence over those in config/application.rb
 
-# Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
-config.action_view.cache_template_loading            = true
+  # The production environment is meant for finished, "live" apps.
+  # Code is not reloaded between requests
+  config.cache_classes = true
 
-config.after_initialize do
-  ExceptionNotifier.exception_recipients = %W(eos8d@virginia.edu mpc3c@virginia.edu)
-  ExceptionNotifier.sender_address = %("Error" <eos8d@virginia.edu>)
-  ExceptionNotifier.email_prefix = "[Blacklight] "
-  ActionMailer::Base.delivery_method = :sendmail
+  # Full error reports are disabled and caching is turned on
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+
+  # Specifies the header that your server uses for sending files
+  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+
+  # For nginx:
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+
+  # If you have no front-end server that supports something like X-Sendfile,
+  # just comment this out and Rails will serve the files
+
+  # See everything in the log (default is :info)
+  # config.log_level = :debug
+
+  # Use a different logger for distributed setups
+  # config.logger = SyslogLogger.new
+
+  # Use a different cache store in production
+  # config.cache_store = :mem_cache_store
+
+  # Disable Rails's static asset server
+  # In production, Apache or nginx will already do this
+  config.serve_static_assets = false
+
+  # Enable serving of images, stylesheets, and javascripts from an asset server
+  # config.action_controller.asset_host = "http://assets.example.com"
+
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
+
+  # Enable threaded mode
+  # config.threadsafe!
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
+  config.i18n.fallbacks = true
+
+  # Send deprecation notices to registered listeners
+  config.active_support.deprecation = :notify
+  
+  config.middleware.use ExceptionNotifier,
+    :email_prefix => "[virgo] ",
+    :sender_address => %{"Error" <mpc3c@virginia.edu>},
+    :exception_recipients => %w{mpc3c@virginia.edu}
+
+  FEEDBACK_NOTIFICATION_RECIPIENTS = %W(virgo-feedback@virginia.edu)
+  FEEDBACK_NOTIFICATION_FROM = 'virgo-feedback@virginia.edu'
+  FEEDBACK_NOTIFICATION_SUBJECT = 'Virgo Feedback (production)'
+
+  FIREHOSE_URL = "http://firehose.lib.virginia.edu:8080/firehose2"
+  PRIMO_URL = "http://primo4.hosted.exlibrisgroup.com:1701/PrimoWebServices/xservice/search/brief?institution=UVA&onCampus=true"
+
 end
 
-# API KEYS FOR UVA #
-LIBRARY_THING_API_KEY = '73d3097200e332adbe542b6eb7fdb162'
-LAST_FM_API_KEY = 'b25b959554ed76058ac220b7b2e0a026'
 
-FEEDBACK_NOTIFICATION_RECIPIENTS = %W(virgo-feedback@virginia.edu)
-FEEDBACK_NOTIFICATION_FROM = 'virgo-feedback@virginia.edu'
-FEEDBACK_NOTIFICATION_SUBJECT = 'Virgo Feedback (production)'
-
-# See everything in the log (default is :info)
-# config.log_level = :debug
-
-# Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
-
-# Use a different cache store in production
-# config.cache_store = :mem_cache_store
-
-# Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host = "http://assets.example.com"
-
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
-
-# route mail through smtp.mail
-config.action_mailer.smtp_settings = {:address => 'smtp.mail.virginia.edu'}
-
-# Enable threaded mode
-# config.threadsafe!
