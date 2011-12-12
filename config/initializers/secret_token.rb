e4a0ff149d35ddcb1ce0b0812c9e051e86e7c289
darthmolly <mpc3c@virginia.edu>
2011-12-12 15:18:52 -0500

    more rails3-ifying

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Virgo::Application.config.secret_token = 'd9626b7def97ba4b41a46d38309d06aba5200be117a756ab77449babfc873c44ae7d7972b9315054b21c321db6c8c2bd7e618592aebe204653e9d11eb9c4d882'
