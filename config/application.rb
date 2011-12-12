912553aed5707928701fec7c3291c5d306140f31
darthmolly <mpc3c@virginia.edu>
2011-12-12 15:18:12 -0500

    more rails3 configs

---
+++ config/application.rb
@@ -0,0 +1,60 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# If you have a Gemfile, require the gems listed there, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env) if defined?(Bundler)
+
+# Wrap error messages with a span of class "fieldWithErrors"
+ActionView::Base.field_error_proc = Proc.new { |html_tag, instance|
+  "<span class=\"fieldWithErrors\">#{html_tag}</span>" }
+
+module Virgo
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Custom directories with classes and modules you want to be autoloadable.
+    # config.autoload_paths += %W(#{config.root}/extras)
+
+    # Only load the plugins named here, in the order given (default is alphabetical).
+    # :all can be used as a placeholder for all plugins not explicitly named.
+    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+    config.plugins = [:all]
+
+    # Activate observers that should always be running.
+    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+    config.active_record.default_timezone = :local
+
+    # JavaScript files you want as :defaults (application.js is always included).
+    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+
+    # Configure the default encoding used in templates for Ruby 1.9.
+    config.encoding = "utf-8"
+
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+    
+#    ENV['RECAPTCHA_PUBLIC_KEY']  = '6Ld3LwkAAAAAAJn9mbxRLerYjUVZFHdjFsYLKsCp'
+#    ENV['RECAPTCHA_PRIVATE_KEY'] = '6Ld3LwkAAAAAAMVxlk5cAzoBCVDp-eQxidkffNFd'
+    
+    # API KEYS FOR UVA #
+#    LIBRARY_THING_API_KEY = '73d3097200e332adbe542b6eb7fdb162'
+#    LAST_FM_API_KEY = 'b25b959554ed76058ac220b7b2e0a026'
+    
+#    FEDORA_REST_URL = 'http://fedora.lib.virginia.edu'
+#    Fedora_username = 'fedoraAdmin'
+#    Fedora_password = 'aro2def'
+    
+  end
+end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_blacklight-uva_session',
-  :secret      => '71a5aa077026b66bfe32287f90d74d0258145e13fed5f9b28e67b201b745e34be5a859207c958d5243a372a1341f1f492658446d79aef365b3ed264c4e1fcd2e'
-}
+Virgo::Application.config.session_store :cookie_store, :key => '_virgo_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# Virgo::Application.config.session_store :active_record_store
