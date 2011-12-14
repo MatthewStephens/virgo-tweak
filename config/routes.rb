a9235e515a7439ca71bf7b97935a236ad7ca2d8e
darthmolly <mpc3c@virginia.edu>
2011-12-14 16:10:39 -0500

    need to set format manually

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   match 'catalog/:id/brief_availability', :to => 'catalog#brief_availability', :as => 'brief_availability'
   match 'catalog/:id/availability', :to => 'catalog#availability', :as => 'availability'
   match 'catalog/:id/image_load', :to => 'catalog#image_load', :as => 'image_load'
-  match 'catalog/:id/firehose', :to => 'catalog#firehose', :as => 'firehose'
+  match 'catalog/:id/firehose', :to => 'catalog#firehose', :as => 'firehose', :defaults => { :format => 'xml'}
   match 'catalog/:id/page_turner', :to => 'catalog#page_turner', :as => 'page_turner'
   match 'fedora_metadata/:id/:pid.:format', :to => 'catalog#fedora_metadata', :as => 'fedora_metadata'
   match 'folder/refworks_texts', :to => 'folder#refworks_texts', :as => 'refworks_texts'
