bf4278626bacf7a377aa82509ff522a68e742b43
darthmolly <mpc3c@virginia.edu>
2012-01-10 16:22:38 -0500

    this should have been here

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,5 @@
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '71a5aa077026b66bfe32287f90d74d0258145e13fed5f9b28e67b201b745e34be5a859207c958d5243a372a1341f1f492658446d79aef365b3ed264c4e1fcd2e'
