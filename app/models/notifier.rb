c0ae02ab15eebeb34676471dbfbf7ec0108d57d7
darthmolly <mpc3c@virginia.edu>
2012-04-27 14:16:42 -0400

    fixing more deprecation warnings around ActionMailer

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -4,10 +4,10 @@ class Notifier < ActionMailer::Base
   def feedback(name, email, message, referer)
     raise 'FEEDBACK_NOTIFICATION_RECIPIENTS is not defined. This should be an array of email recipients' unless defined?(FEEDBACK_NOTIFICATION_RECIPIENTS)
     # Email header info MUST be added here
-    recipients FEEDBACK_NOTIFICATION_RECIPIENTS # set in config/environment.rb
-    from FEEDBACK_NOTIFICATION_FROM
-    subject FEEDBACK_NOTIFICATION_SUBJECT
-    body <<-EOF
+    @recipients = FEEDBACK_NOTIFICATION_RECIPIENTS # set in config/environment.rb
+    @from = FEEDBACK_NOTIFICATION_FROM
+    @subject = FEEDBACK_NOTIFICATION_SUBJECT
+    @body = "
 VIRGO USER FEEDBACK
 
 SENDER NAME: #{name}
@@ -19,7 +19,8 @@ SENDER MESSAGE:
 
 REFERING URL:
 #{referer}
-EOF
+"
+  mail(:to => @recipients, :subject => @subject, :from => @from, :body => @body)
   end
   
 end
