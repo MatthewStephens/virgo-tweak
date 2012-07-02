b10cbef846db4434c9a59d8cfc8fffce2406623d
Xiaoming Wang <xw5d@virginia.edu>
2012-07-02 17:06:26 -0400

    require login before users reach the course reserve form and redirct to reserves email form when there's error in it LIBSRVSRCHDISCOV-409

--- app/controllers/reserves_controller.rb
+++ app/controllers/reserves_controller.rb
@@ -1,18 +1,18 @@
-require 'lib/uva/articles_helper'
 require 'lib/uva/solr_helper_override'
+require 'lib/firehose/patron'
+
 
 class ReservesController < ApplicationController
-  include UVA::ArticlesHelper
   include Blacklight::SolrHelper
   include Blacklight::Configurable
   include UVA::SolrHelperOverride
-  
-  
+  include Firehose::Patron
+
   before_filter :max_per_page_params, :only=>[:index, :refworks_texts, :csv]
   before_filter :resolve_sort, :only=>:index
-  before_filter :articles, :only=>[:index, :csv]
   
  def cres_email
      @response, @documents = get_solr_response_for_field_values("id",session_folder_document_ids || []) 
+     @user_patron = get_patron(current_user.login) unless current_user.blank?
  end
 end
